574b7ad0eda363f366e20f7e09c84d3063837cdf
Bartosz Zimończyk <bartosz.zimonczyk@gmail.com>
2022-12-11 16:12:18 +0100

    fixed scripts for BEM2

--- simulate_bem2.sh
+++ simulate_bem2.sh
@@ -1,4 +1,4 @@
-sbatch
+cat << EOF | sbatch
 #!/bin/bash
 #SBATCH --time=4:00:00
 #SBATCH --job-name="job_$2"
@@ -10,8 +10,12 @@ sbatch
 # go to the mgr directory to execute Rscript properly
 cd mgr
 
+# load modules
+source /usr/local/sbin/modules.sh
+
 # load R version
 module load R/3.6.2-foss-2019b >& load_output.txt
 
 # execute given R script
 Rscript $1 >& Rscript_output.txt
+EOF
