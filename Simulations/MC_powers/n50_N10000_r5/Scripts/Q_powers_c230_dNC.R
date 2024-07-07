983c01c5ddc5bcb3e0e893f66b9e5b9199f37e3f
Bartosz Zimończyk <bartosz.zimonczyk@gmail.com>
2024-07-07 14:21:49 +0200

    Quick push by script.

--- Simulations/MC_powers/n50_N10000_r5/Scripts/Q_powers_c230_dNC.R
+++ Simulations/MC_powers/n50_N10000_r5/Scripts/Q_powers_c230_dNC.R
@@ -13,14 +13,6 @@ get_this_file <- function(){
 # automating simulations by changing control to naming
 # given folders and files
 
-# testing by hand
-# n <- 50
-# N <- 100
-# r <- 5
-# c_t <- 2.3
-# distribution <- "N2B2"
-# this_file_name <- "Q_powers_c230_dN2B2.R"
-
 path <- get_this_file()
 print(path)
 path_splitted <- strsplit(path, "/")[[1]]
@@ -39,6 +31,14 @@ r <- parameters[3]
 distribution <- unlist(strsplit(this_file_name, "_"))[4]
 distribution <- substring(distribution, 2, nchar(distribution) - 2)
 c_t <- as.double(substr(this_file_name, 11, 13)) / 100
+
+# testing by hand
+# n <- 50
+# N <- 100
+# r <- 5
+# c_t <- 2.3
+# distribution <- "Cauchy"
+# this_file_name <- "Q_powers_c230_dCauchy.R"
 
 
 cat("Sanity check of hyperparameters: \n")
@@ -334,36 +334,36 @@ write.csv(
 rule_names <- c("A", "T.A", "S", "T.M", "M")
 order_of_ls <- c(17,9,18,5,19,10,20,3,21,11,22,6,23,12,24,2,25,13,26,7,27,14,28,4,29,15,30,8,31,16,32,1)
 
-png(paste("Simulations/MC_powers/", this_folder_name, "/Plots/Mean_ls_c", round(c_t*100, 0) ,"_d", distribution, ".png", sep=""), height=900, width=600)
-par(mfrow=c(5,1))
-for(i in 1:5){
-  b = barplot(
-    names.arg = round(p(order_of_ls), 2),
-    df_ls_no_zeros[order_of_ls, i],
-    ylim = c(0, max(df_ls_no_zeros)+1.1),
-    yaxt='n',
-    main=paste("Barplot of mean values of lj for rule", rule_names[i]),
-    xlab = expression(paste("The end point of interval in wich we are checking assymetry i.e., [0, ", phi, "(j)], in increasing order", sep="")),
-    ylab = "Mean",
-    las=2
-  )
-  text(b, df_ls_no_zeros[order_of_ls, i]+0.85, labels=paste(as.character(round(df_ls_no_zeros[order_of_ls ,i], 2))))
-}
-
+png(paste("Simulations/MC_powers/", this_folder_name, "/Plots/Mean_ls_c", round(c_t*100, 0) ,"_d", distribution, ".png", sep=""), height=1200, width=800)
 # par(mfrow=c(5,1))
 # for(i in 1:5){
 #   b = barplot(
 #     names.arg = round(p(order_of_ls), 2),
-#     df_ls[order_of_ls, i],
-#     ylim = c(0, max(df_ls)+1.1),
+#     df_ls_no_zeros[order_of_ls, i],
+#     ylim = c(0, max(df_ls_no_zeros)+1.1),
 #     yaxt='n',
 #     main=paste("Barplot of mean values of lj for rule", rule_names[i]),
 #     xlab = expression(paste("The end point of interval in wich we are checking assymetry i.e., [0, ", phi, "(j)], in increasing order", sep="")),
 #     ylab = "Mean",
 #     las=2
 #   )
-#   text(b, df_ls[order_of_ls, i]+0.85, labels=paste(as.character(round(df_ls[order_of_ls ,i], 2))))
+#   text(b, df_ls_no_zeros[order_of_ls, i]+0.85, labels=paste(as.character(round(df_ls_no_zeros[order_of_ls ,i], 2))))
 # }
+
+par(mfrow=c(5,1))
+for(i in 1:5){
+  b = barplot(
+    names.arg = round(p(order_of_ls), 2),
+    df_ls[order_of_ls, i] / sqrt(n),
+    ylim = c(0, max(df_ls/sqrt(n))+1.1),
+    yaxt='n',
+    main=paste("Barplot of mean values of lj for rule", rule_names[i]),
+    xlab = expression(paste("The end point of interval in wich we are checking assymetry i.e., [0, ", phi, "(j)], in increasing order", sep="")),
+    ylab = "Mean",
+    las=2
+  )
+  text(b, df_ls[order_of_ls, i]/sqrt(n)+0.5, labels=paste(as.character(round(df_ls[order_of_ls ,i]/sqrt(n), 2))), srt=60)
+}
 
 dev.off()
 
